5bf3d8475e1f5d7c8575ba3f59f33c4e874af2c5
sousic <517048082@qq.com>
2019-10-11 08:29:50 +0800

    Hw3

---
+++ Hw3/gauss.c
@@ -0,0 +1,186 @@
+#include <math.h>
+#include <time.h>
+#include "stdio.h"
+#include <stdlib.h>
+#include "mpi.h"
+
+const int rows = 3; /*the rows of matrix*/
+const int cols = 3; /*the cols of matrix*/
+
+int main(int argc, char **argv)
+{
+	int i,j,k,myid,numprocs,anstag;
+	double A[rows][cols],B[cols],X[rows],AB[rows][cols+1];
+	int masterpro;
+	double buf[cols+1];
+	double starttime,endtime;
+	double tmp,totaltime;
+
+	srand((unsigned int)time(NULL));
+
+	MPI_Status status;
+
+	masterpro = 0;
+
+	MPI_Init(&argc,&argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+
+	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+
+	if (myid==0)
+	{
+		starttime = MPI_Wtime();
+		printf("please input coefficient:\n");
+		for(i=0;i<cols;i++)
+		{//get A
+			for(j=0;j<rows;j++)
+			{
+				scanf("%lf",&A[i][j]);
+			}
+		}
+		printf("please input constant:\n");
+		for(i=0;i<rows;i++)
+		{//get B
+			scanf("%lf",&B[i]);
+		}
+		for(i=0;i<rows;i++)
+		{//get augmented matrix AB
+			AB[i][cols]=B[i];
+			for(j=0;j<cols;j++)
+				AB[i][j]=A[i][j];
+		}
+		printf("augmented matrix:\n");
+		for(i=0;i<rows;i++)
+		{//print augmented matrix AB
+			for(j=0;j<cols+1;j++)
+				printf("%.2f ",AB[i][j]);
+			printf("\n");
+		}	
+	}
+
+	int x;
+	double coe;
+
+	for(x=0;x<rows;x++)
+	{
+		MPI_Bcast(&AB[x][0],cols+1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+		if(myid==0)
+		{
+			for(i=1;i<numprocs;i++)
+			{
+				for(k=x+1+i;k<rows;k+=numprocs)
+				MPI_Send(&AB[k][0],cols+1,MPI_DOUBLE,i,0,MPI_COMM_WORLD);
+			}
+			for(k=x+1;k<rows;k+=numprocs)
+			{
+				coe=AB[k][x]/AB[x][x];
+				for(j=x;j<cols+1;j++)
+				{
+					AB[k][j]-=AB[x][j]*coe;
+				}
+			}
+			for(i=1;i<numprocs;i++)
+			{
+				for(k=x+1+i;k<rows;k+=numprocs)
+				{
+					MPI_Recv(&AB[k][0],cols+1,MPI_DOUBLE,i,1,MPI_COMM_WORLD,&status);
+				}
+			}
+		}
+		else
+		{
+			for(k=x+1+myid;k<rows;k+=numprocs)
+			{
+				MPI_Recv(&AB[k-1][0],cols+1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,&status);
+				coe=AB[k-1][x]/AB[x][x];
+				for(j=x;j<cols+1;j++)
+				{
+					AB[k-1][j]-=AB[x][j]*coe;
+				}
+				MPI_Send(&AB[k-1][0],cols+1,MPI_DOUBLE,0,1,MPI_COMM_WORLD);
+			}
+		}
+	}
+
+	if(myid==0)
+	{
+		printf("upper triangle:\n");
+		for(i=0;i<rows;i++)
+		{//print AB after elimination
+			for(j=0;j<cols+1;j++)
+				printf("%.2f ",AB[i][j]);
+			printf("\n");
+		}
+	}
+
+	int result;
+	if(myid==0)
+	{
+		if(fabs(AB[rows-1][cols-1])<0.0000001 && fabs(AB[rows-1][cols]<0.0000001))
+		{
+			printf("ifinite solution\n");
+			result=1;
+		}
+		if(fabs(AB[rows-1][cols-1])<0.0000001 && fabs(AB[rows-1][cols]>0.0000001))  
+		{
+			printf("no solution\n");
+			result=-1;
+		}
+		else
+		{
+			printf("unique solution\n");
+			result=0;
+		}
+	}
+
+	double temp=0.0;
+
+	if(result==0)
+	{
+		if(myid==0)
+		{
+			X[rows-1]=AB[rows-1][cols]/AB[rows-1][cols-1];
+			for(k=rows-2;k>=0;k--)
+			{
+				temp=0.0;
+				for (j=k+1;j<cols;j++)
+					temp=temp+AB[k][j]*X[j];
+				X[k]=(AB[k][cols]-temp)/AB[k][k];
+			}
+			printf("x:\n");
+			for(i=0;i<rows;i++)
+				printf("X[%d]=%.2f\n",i,X[i]);
+		}
+	}
+
+	if(result==1)
+	{
+		if(myid==0)
+		{
+			X[rows-1]=0;
+			for(k=rows-2;k>=0;k--)
+			{
+				temp=0;
+				for (j=k+1;j<cols;j++)
+					temp=temp+AB[k][j]*X[j];
+				X[k]=(AB[k][cols]-temp)/AB[k][k];
+			}
+			printf("x:\n");
+			for(i=0;i<rows;i++)
+				printf("X[%d]=%.2f\n",i,X[i]);
+		}
+	}
+
+	endtime = MPI_Wtime();
+
+	totaltime = endtime - starttime;
+
+	if (myid == masterpro)
+	printf("total time :%f s.\n",totaltime);
+
+	MPI_Finalize();
+
+	return 0;
+}
+
---
+++ Hw3/gauss2.cpp
@@ -0,0 +1,108 @@
+/**yhf at YUQUAN**/
+#include "mpi.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+const int N = 2000;
+int myid, numproc;
+void copyMemory(const double *M, double *A) {//复制矩阵使得同个进程中需要的内存是连续的
+	for (int i = 0; i < N*(N + 1); i++)
+		A[((i / (N + 1)) % numproc)*( (N * (N + 1)) / numproc) + (N+1) * ((i / (N + 1)) / numproc) + i % (N + 1)] = M[i];
+}
+int main(int argc,char* argv[]) {
+	MPI_Init(&argc,&argv);
+	double t1, t2;
+	t1 = MPI_Wtime();
+	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+	MPI_Comm_size(MPI_COMM_WORLD,&numproc);
+	double *M, *c, *M_trans,*A,*tempRow;
+	int *map = new int[N]; int *rmap = new int[N];
+	for (int i = 0; i < N; i++) {
+		map[i] = -1; rmap[i] = -1;
+	}
+	if (myid == 0) {//内存分配
+		M = new double[N*(N+1)];
+		M_trans = new double[N*(N + 1)];
+		for (int i = 0; i < N*(N + 1); i++) {
+			M[i] = rand() % 10;
+		}
+		
+		copyMemory(M, M_trans);
+		A = new double[N*(N + 1) / numproc + 1];
+		c = new double[N];
+	}
+	else {
+		M = new double[1];
+		M_trans = new double[1];
+		A = new double[N*(N + 1) /numproc+1];
+		c = new double[N];
+	}
+	tempRow = new double[N + 1];
+	MPI_Scatter(M_trans, N*(N+1) / numproc,MPI_DOUBLE,A,N*(N+1)/numproc,MPI_DOUBLE,0,MPI_COMM_WORLD);//分发矩阵
+	delete M_trans;
+	double local_max_value, global_max_value;
+	int local_max_id, max_proc_id, max_pro_id_1, global_max_id;
+	for (int row = 0; row < N-1; row++) {
+		local_max_value = -1e10; local_max_id = -1;  max_pro_id_1 = -1, global_max_id=-1;
+		for (int i = 0; i < N / numproc; i++) {
+			if (rmap[i * numproc + myid] < 0) {
+				if (A[i*(N + 1) + row] > local_max_value) {
+					local_max_value = A[i*(N + 1) + row];
+					local_max_id = i;
+				}
+			}
+		}//找到对应进程的主元
+		MPI_Allreduce(&local_max_value, &global_max_value, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);//将每个进程的主元找到一个最大值
+		if (global_max_value > local_max_value-0.00001&&global_max_value < local_max_value+0.00001) {//对比最大值找到主元
+			max_pro_id_1 = myid;
+			for (int i = 0; i < N + 1; i++) {
+				tempRow[i] = A[local_max_id*(N + 1) + i];
+			}
+			global_max_id= local_max_id * numproc + myid;			
+		}
+		MPI_Allreduce(&max_pro_id_1,&max_proc_id,1,MPI_INT,MPI_MAX,MPI_COMM_WORLD);//广播主元所在的进程便于下面的操作
+		MPI_Bcast(&max_proc_id,1,MPI_INT,0,MPI_COMM_WORLD);
+		MPI_Bcast(tempRow,N+1,MPI_DOUBLE,max_proc_id,MPI_COMM_WORLD);//广播主元行
+		MPI_Bcast(&global_max_id,1,MPI_INT,max_proc_id,MPI_COMM_WORLD);//广播主元id
+		
+		map[row] = global_max_id; rmap[global_max_id] = row;
+		for (int i = 0; i < N/numproc; i++) {//求解过程
+			if (rmap[i * numproc + myid] < 0) {
+				double temp = A[i*(N + 1)+row] / tempRow[row];
+				for (int j = row; j < N + 1; j++) {
+					A[i*(N + 1) + j] -= tempRow[j] * temp;
+				}
+			}
+		}
+	}
+	
+	MPI_Gather(A,N*(N+1)/numproc,MPI_DOUBLE,M,N*(N+1)/numproc,MPI_DOUBLE,0,MPI_COMM_WORLD);//求解完三角阵后规约起来
+	if (myid == 0) {
+		for (int i = 0; i < N; i++) {
+			if (rmap[i] == -1) {
+				map[N - 1] = i;
+			}
+		}
+		printf("\n");
+	}
+	if (myid == 0) {	//输出
+		for (int i = N-1; i >=0; i--) {
+			int index = map[i] % numproc*(N / numproc) + map[i] / numproc;
+			for (int j = N-1; j >i; j--) {
+				M[index*(N+1)+N] -= c[j] * M[index*(N + 1) + N - j];
+			}
+			c[i] = M[index*(N + 1) + N] / M[index*(N+1)+i];
+		}
+		for (int i = 0; i < N; i++) {
+			printf("C[%d]=%.2f\n",i, c[i]);
+		}
+	}
+	t2 = MPI_Wtime();
+	printf("It take %.2lfs\n", t2-t1);
+	MPI_Finalize();
+}
+
